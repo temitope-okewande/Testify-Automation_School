8d147106004af41ef4bb6f43cfa5129e493a6e33
Temitope Okewande <Topehnics@gmail.com>
2024-03-20 23:02:42 +0100

    Javascript fun fact task

---
+++ Module 3/3A(JavaScript)/javascript _task2.js
@@ -0,0 +1,5 @@
+//JavaScript took just 10 days to develop.
+//javascript and java are two different and opposite programming languages.
+//Javascipt Runs everywhere
+//Javascipt is the most popular language
+//JavaScript is a dynamically typed language
