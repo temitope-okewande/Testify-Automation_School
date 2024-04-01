02d7128b29bc30eda9b17c494b44214b0ed79916
Temitope Okewande <Topehnics@gmail.com>
2024-04-01 20:12:24 +0100

    changes update

--- module_3/3a_javascript/task13.js
+++ module_3/3a_javascript/task13.js
@@ -3,16 +3,8 @@ const book ={
     description :'My book description', 
     numberOfPages : 2,
     authour : 'Temitope Okewande',
-    reading : true,
-    toggleReadingStatus : function(){
-        if (book.reading === true){
-            book.reading = false
-        }
-        else{
-            book.reading = true
-        }
-
-    }
+    reading : true
+    
 
 }
 console.log(book)
---
+++ module_3/3a_javascript/task14.js
@@ -0,0 +1,18 @@
+const book ={
+    title : 'My book',
+    description :'My book description', 
+    numberOfPages : 2,
+    authour : 'Temitope Okewande',
+    reading : true,
+    toggleReadingStatus : function(){
+        if (book.reading === true){
+            book.reading = false
+        }
+        else{
+            book.reading = true
+        }
+
+    }
+
+}
+console.log(book.toggleReadingStatus)
