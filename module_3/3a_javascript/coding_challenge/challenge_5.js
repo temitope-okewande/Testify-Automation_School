c0a120b89a702f26dd76f334c539ed561e0fda78
Temitope Okewande <Topehnics@gmail.com>
2024-04-10 17:52:45 +0100

    challenge 6

---
+++ module_3/3a_javascript/coding_challenge/challenge_5.js
@@ -0,0 +1,10 @@
+//Create a function that reverses an array.
+function array_reverse(){
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits.sort();
+    answer = fruits.reverse();
+    console.log(answer)
+
+
+}
+array_reverse()
