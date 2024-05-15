a649e57cda20db5e878ff80316bd34380c16d323
Temitope Okewande <Topehnics@gmail.com>
2024-05-15 17:45:50 +0100

    Task 2

---
+++ module_4/task/task2.java
@@ -0,0 +1,16 @@
+package task;
+
+public class task2 {
+    public static void main(String[] args) {
+        // Data Types
+        byte age =120;
+        short negative = -32_742;
+        long currency = 214748364L;
+        float vote = 1234567.90f;
+        double population = 123456789.123;
+        char gender = 'M';
+        boolean answer = true;
+
+
+    }
+}
