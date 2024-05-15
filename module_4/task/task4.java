bffc4b3633d9238efbc11f4d5ebd3839dfaefd39
Temitope Okewande <Topehnics@gmail.com>
2024-05-15 20:22:19 +0100

    radius value change

--- module_4/task/task4.java
+++ module_4/task/task4.java
@@ -4,7 +4,7 @@ public class task4 {
     public static void main(String[] args) {
         //Calculate area of a circle
         double pie = 3.142;
-        int radius = 3;
+        double radius = 12.7;
         double radius_square = Math.pow(radius, 2);
         double area = pie * radius_square;
         char unit = 'm';
