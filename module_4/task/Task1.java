971f766155a96d1ba9b28263eff10ef225d1ac48
Temitope Okewande <Topehnics@gmail.com>
2024-05-18 20:01:30 +0100

    changes

---
+++ module_4/task/Task1.java
@@ -0,0 +1,8 @@
+package task;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Great");
+        System.out.println("Me");
+    }
+}
--- module_4/task/Task5.java
+++ module_4/task/Task5.java
@@ -13,12 +13,12 @@ public class Task5 {
 
         double number = Double.parseDouble(numberInString);
         
-        if (number % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (number % 5 ==0 ) {
-            System.out.println("Buzz");
-        }else if((number % 3 == 0) && (number % 5 == 0)) {
+        if ((number % 3 == 0) && (number % 5 == 0)) {
             System.out.println("FizzBuzz");
+        } else if (number % 3 == 0 ) {
+            System.out.println("Fizz");
+        }else if( number % 5 == 0) {
+            System.out.println("Buzz");
         }else {
             System.out.println(numberInString);
         }
