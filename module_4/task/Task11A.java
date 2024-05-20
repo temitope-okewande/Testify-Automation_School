34b6d23fc565ea901c307ad591dedbda0fe588aa
Temitope Okewande <Topehnics@gmail.com>
2024-05-20 18:03:16 +0100

    task 11

---
+++ module_4/task/Task11A.java
@@ -0,0 +1,9 @@
+package task;
+
+public class Task11A {
+    String name;
+    public void my_name (String n){
+        name = n;
+        System.out.println("My name is " + name);
+    }
+}
---
+++ module_4/task/Task11B.java
@@ -0,0 +1,10 @@
+package task;
+
+public class Task11B {
+    public static void main(String[] args) {
+        Task11A temitopeVariable = new Task11A();
+        temitopeVariable.name = "Temitope";
+        temitopeVariable.my_name("Temitope");
+    }
+
+}
