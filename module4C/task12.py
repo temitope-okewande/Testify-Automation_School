f852d87443a93019a3acaa272a0b81c8e9cdde11
Temitope Okewande <Topehnics@gmail.com>
2024-07-06 19:25:43 +0100

    correctiom

--- module4C/task12.py
+++ module4C/task12.py
@@ -1,15 +1,17 @@
 # Declare a global variable
 language = "Python"
 
+
 # Define a function that declares a local variable with the same name
-def language():
+def myLanguage():
     # Declare a local variable with the same name
     language = "Java"
     # Print the local variable
     print("Inside the function, language:", language, "\n")
 
+
 # Invoke the function
-language()
+myLanguage()
 
 # Print the global variable
 print("Outside the function, language:", language)
